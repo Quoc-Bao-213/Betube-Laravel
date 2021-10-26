aaca2ff553fdfbbb021f0165e1915a11f959c97b
Quoc-Bao-213 <Quoc-Bao-213@users.noreply.github.com>
2021-10-26 14:07:43 +0700

    Betube: remove template Admin

--- resources/views/admin/auth/link-reset-pass-admin.blade.php
+++
@@ -1,5 +0,0 @@
-<p>We heard that you lost your BeTube password. Sorry about that!</p>
-<p>But don’t worry! You can use the following link to reset your password:</p>
-<p>{{URL::to('/admin')}}/form-reset?email={{ $email }}</p>
-<p>Thanks,<br>
-The BeTube Team</p>
