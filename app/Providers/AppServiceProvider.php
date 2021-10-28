36ce59b2071506c08267771e3d9262d9376af7e2
Quoc-Bao-213 <Quoc-Bao-213@users.noreply.github.com>
2021-10-28 14:10:04 +0700

    Betube: update footer and ServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,10 @@
 
 namespace App\Providers;
 
+use App\Video;
+use App\VideoType;
+use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +27,15 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        if(config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
+        
+        View::composer('betube.partials.footer', function ($view) {
+            $view->with([
+                'categories' => VideoType::all(),
+                'latestVideos' => Video::where('percentage', 100)->latest()->take(3)->get()
+            ]);
+        });
     }
 }
--- resources/views/betube/partials/footer.blade.php
+++ resources/views/betube/partials/footer.blade.php
@@ -7,7 +7,7 @@
                     <h5>About Betube</h5>
                 </div>
                 <div class="textwidget">
-                    Betube video wordpress theme lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s book.
+                    BeTube is a online video sharing and social media platform. It was launched in October 2021 by Quoc Bao.
                 </div>
             </div>
         </div>
@@ -17,54 +17,24 @@
                     <h5>Recent Videos</h5>
                 </div>
                 <div class="widgetContent">
-                    <div class="media-object">
-                        <div class="media-object-section">
-                            <div class="recent-img">
-                                <img src= "{{ asset('images/recent1.png') }}" alt="recent">
-                                <a href="#" class="hover-posts">
-                                    <span><i class="fa fa-play"></i></span>
-                                </a>
+                    @foreach ($latestVideos as $video)
+                        <div class="media-object">
+                            <div class="media-object-section">
+                                <div class="recent-img">
+                                    <img src= "{{ asset($video->thumbnail) }}" alt="recent">
+                                    <a href="{{ route('video', $video->id ) }}" class="hover-posts">
+                                        <span><i class="fa fa-play"></i></span>
+                                    </a>
+                                </div>
+                            </div>
+                            <div class="media-object-section">
+                                <div class="media-content">
+                                    <h6><a href="{{ route('video', $video->id ) }}">{{ $video->title }}</a></h6>
+                                    <p><i class="fa fa-user"></i><span>{{ $video->user->channel_name }}</span><i class="fa fa-clock-o"></i><span>{{ $video->created_at->toFormattedDateString() }}</span></p>
+                                </div>
                             </div>
                         </div>
-                        <div class="media-object-section">
-                            <div class="media-content">
-                                <h6><a href="#">The lorem Ipsumbeen the industry's standard.</a></h6>
-                                <p><i class="fa fa-user"></i><span>admin</span><i class="fa fa-clock-o"></i><span>5 january 16</span></p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="media-object">
-                        <div class="media-object-section">
-                            <div class="recent-img">
-                                <img src= "{{ asset('images/video-thumbnail/1.jpg') }}" alt="recent">
-                                <a href="#" class="hover-posts">
-                                    <span><i class="fa fa-play"></i></span>
-                                </a>
-                            </div>
-                        </div>
-                        <div class="media-object-section">
-                            <div class="media-content">
-                                <h6><a href="#">The lorem Ipsumbeen the industry's standard.</a></h6>
-                                <p><i class="fa fa-user"></i><span>admin</span><i class="fa fa-clock-o"></i><span>5 january 16</span></p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="media-object">
-                        <div class="media-object-section">
-                            <div class="recent-img">
-                                <img src= "{{ asset('images/recent3.png') }}" alt="recent">
-                                <a href="#" class="hover-posts">
-                                    <span><i class="fa fa-play"></i></span>
-                                </a>
-                            </div>
-                        </div>
-                        <div class="media-object-section">
-                            <div class="media-content">
-                                <h6><a href="#">The lorem Ipsumbeen the industry's standard.</a></h6>
-                                <p><i class="fa fa-user"></i><span>admin</span><i class="fa fa-clock-o"></i><span>5 january 16</span></p>
-                            </div>
-                        </div>
-                    </div>
+                    @endforeach
                 </div>
             </div>
         </div>
@@ -74,17 +44,9 @@
                     <h5>Tags</h5>
                 </div>
                 <div class="tagcloud">
-                    <a href="#">3D Videos</a>
-                    <a href="#">Videos</a>
-                    <a href="#">HD</a>
-                    <a href="#">Movies</a>
-                    <a href="#">Sports</a>
-                    <a href="#">3D</a>
-                    <a href="#">Movies</a>
-                    <a href="#">Animation</a>
-                    <a href="#">HD</a>
-                    <a href="#">Music</a>
-                    <a href="#">Recreation</a>
+                    @foreach ($categories as $category)
+                        <a href="{{ route('categories', $category->id) }}">{{ $category->name }}</a>
+                    @endforeach
                 </div>
             </div>
         </div>
@@ -97,6 +59,6 @@
         <img src="{{ asset('images/footerlogo.png') }}" alt="footer logo">
     </div>
     <div class="btm-footer-text text-center">
-        <p>2016 © Betube video wordpress theme.</p>
+        <p>2021 © BeTube video</p>
     </div>
 </div>
