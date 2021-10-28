e9aa2fcad95404105b54f1d09dc5c53418cf37bc
Quoc-Bao-213 <Quoc-Bao-213@users.noreply.github.com>
2021-10-28 17:13:26 +0700

    Betube: update seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -16,56 +16,80 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $user1 = factory(User::class)->create([
-            'email' => 'voez210399@gmail.com'
-        ]);
+        // $user1 = factory(User::class)->create([
+        //     'email' => 'voez210399@gmail.com'
+        // ]);
 
-        $user2 = factory(User::class)->create([
-            'email' => 'coderb2103@gmail.com'
-        ]);
+        // $user2 = factory(User::class)->create([
+        //     'email' => 'coderb2103@gmail.com'
+        // ]);
 
-        $user1->subscriptions()->create([
-            'user_subscribe' => $user2->id
-        ]);
+        // $user1->subscriptions()->create([
+        //     'user_subscribe' => $user2->id
+        // ]);
 
-        $user2->subscriptions()->create([
-            'user_subscribe' => $user1->id
-        ]);
+        // $user2->subscriptions()->create([
+        //     'user_subscribe' => $user1->id
+        // ]);
 
-        factory(User::class)->create([
-            'email' => 'taquocbao.it.99@gmail.com',
-            'is_admin' => true
-        ]);
+        // factory(User::class)->create([
+        //     'email' => 'taquocbao.it.99@gmail.com',
+        //     'is_admin' => true
+        // ]);
 
-        factory(Subscription::class, 10)->create([
-            'user_id' => $user1->id,
-        ]);
+        // factory(Subscription::class, 10)->create([
+        //     'user_id' => $user1->id,
+        // ]);
 
-        factory(Subscription::class, 10)->create([
-            'user_id' => $user2->id,
-        ]);
+        // factory(Subscription::class, 10)->create([
+        //     'user_id' => $user2->id,
+        // ]);
 
-        factory(VideoType::class)->create([
+        // factory(VideoType::class)->create([
+        //     'name' => "Music"
+        // ]);
+
+        // factory(VideoType::class)->create([
+        //     'name' => "Technology"
+        // ]);
+
+        // factory(VideoType::class)->create([
+        //     'name' => "News"
+        // ]);
+
+        // factory(VideoType::class)->create([
+        //     'name' => "Movies"
+        // ]);
+
+        // factory(VideoType::class)->create([
+        //     'name' => "Entertainment"
+        // ]);
+
+        // factory(VideoType::class)->create([
+        //     'name' => "Game"
+        // ]);
+
+        VideoType::create([
             'name' => "Music"
         ]);
 
-        factory(VideoType::class)->create([
+        VideoType::create([
             'name' => "Technology"
         ]);
 
-        factory(VideoType::class)->create([
+        VideoType::create([
             'name' => "News"
         ]);
 
-        factory(VideoType::class)->create([
+        VideoType::create([
             'name' => "Movies"
         ]);
 
-        factory(VideoType::class)->create([
+        VideoType::create([
             'name' => "Entertainment"
         ]);
-
-        factory(VideoType::class)->create([
+        
+        VideoType::create([
             'name' => "Game"
         ]);
 
